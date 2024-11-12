f07c5464587ce2aeb8f6f4ac35297e1990d8cdc4
zduka <zduka@outlook.com>
2024-11-12 22:11:19 +0100

    Memory heap & arena growing against each other. Tests pass.

--- sdk/rckid/memory.cpp
+++ sdk/rckid/memory.cpp
@@ -1,5 +1,6 @@
 #include "rckid.h"
 
+// Heap is continuous space from _bss_end__ to __StackLimit, heap grows from __StackLimit down towards _bss_end, while the arena grows from __bss_end_ up. 
 #if (defined ARCH_FANTASY)
     char fantasyHeap[520 * 1024];
     char & __bss_end__ = *fantasyHeap;
@@ -20,7 +21,7 @@ namespace {
         Each chunk contains its size, followed by the actual data of the chunk (the user available allocated memory )
      */
     PACKED(struct Chunk {
-        uint32_t size;
+        ChunkHeader size;
         Chunk * next;
 
         Chunk(uint32_t size): size{size} {}
@@ -43,21 +44,24 @@ namespace {
 
     static_assert(sizeof(Chunk) == sizeof(ChunkHeader) + sizeof(Chunk*));
 
-    /** Memory Arena
-     
-        Contains a pointer to the previous arena and its own freelist. so that when the arena is removed, previous freelist can be restored.
-     */
-    struct Arena {
-        Chunk * freelist = nullptr;
+    Chunk * freelist = nullptr;
+
+    // heap starts at __StackLimit and grows down towards __bss_end_. This is the pointer to the end (bottom) of the heap. 
+    char * heapEnd = & __StackLimit;
+
+
+    PACKED(struct Arena {
         Arena * previous;
+    });
 
-        explicit Arena(Arena * prev): previous{prev} {}
-    }; 
+    static_assert(sizeof(Arena) == sizeof(Arena*));
 
-    static_assert(sizeof(Arena) == sizeof(void*) * 2);
+    // arena starts at the end of bss and grows up towards the heap. This is the end of the arena, i.e. place of next arena allocation. 
+    char * arenaEnd = & __bss_end__;
 
-    Arena * arena = new (__builtin_assume_aligned(&__bss_end__, 8)) Arena{nullptr};
-    char * heapEnd = & __bss_end__ + sizeof(Arena);
+    // beginning of the current arena. Anything from arenastart to arena end will reclaimed when the leaving. 
+    Arena * arenaStart = nullptr;
+
 
     uint32_t mallocCalls = 0;
     uint32_t freeCalls = 0;
@@ -67,13 +71,13 @@ namespace {
 extern "C" {
     void *__wrap_malloc(size_t numBytes) {
         ++mallocCalls;
-        Chunk * freeChunk = arena->freelist;
+        Chunk * freeChunk = freelist;
         Chunk * last = nullptr;
         // see if we can use a chunk from freelist
         while (freeChunk != nullptr) {
             if (freeChunk->size >= numBytes) {
                 if (last == nullptr)
-                    arena->freelist = freeChunk->next;
+                    freelist = freeChunk->next;
                 else 
                     last->next = freeChunk->next;
                 TRACE_MEMORY("allocating " << numBytes<< " from existing chunk");
@@ -83,10 +87,10 @@ extern "C" {
             freeChunk = freeChunk->next;
         }
         // we haven't found anything in the freelist, use the end of the heap to create one and advance the heap end
+        heapEnd -= (numBytes + sizeof(ChunkHeader));
         Chunk * result = (Chunk*) heapEnd;
-        heapEnd += numBytes + sizeof(ChunkHeader); 
         // if we are over the limit, panic
-        ASSERT(heapEnd <= & __StackLimit);
+        ASSERT(heapEnd >= arenaEnd);
         // set the chunk's size and return it 
         result->size = static_cast<uint32_t>(numBytes);
         TRACE_MEMORY("allocating " << numBytes<< " from heap, free " << rckid::memoryFreeHeap());
@@ -98,20 +102,20 @@ extern "C" {
         if (ptr == nullptr)
             return;
         // check that we are freeing memory that is higher than the current arena, otherwise we are freeing from a previous arena which is wrong
-        ASSERT(ptr > arena); 
+        ASSERT(ptr > heapEnd); 
         ++freeCalls;
         // deal with the chunk
         Chunk * chunk = (Chunk *)((char*) ptr - 4);
         // if this is the last allocated memory chunk, simply update the heap end
-        if (chunk->end() == heapEnd) {
-            heapEnd = chunk->start();
+        if (chunk->start() == heapEnd) {
+            heapEnd = chunk->end();
             TRACE_MEMORY("deallocating last chunk, free " << rckid::memoryFreeHeap());
             return;
         }
         // get the chunk and prepend it to the freelist
         // TODO this is extremely ugly and inefficient, must be fixed in the future
-        chunk->next = arena->freelist;
-        arena->freelist = chunk;
+        chunk->next = freelist;
+        freelist = chunk;
         TRACE_MEMORY("deallocating and adding to freelist, free " << rckid::memoryFreeHeap());
     }
 
@@ -120,38 +124,53 @@ extern "C" {
 namespace rckid {
 
     uint32_t memoryFreeHeap() {
-        return static_cast<uint32_t>(& __StackLimit - heapEnd);
+        ASSERT(heapEnd >= arenaEnd);
+        return static_cast<uint32_t>(heapEnd - arenaEnd);
     }
     uint32_t memoryUsedHeap() {
         return static_cast<uint32_t>(heapEnd - & __bss_end__);
     }
 
     bool memoryIsOnHeap(void * ptr) {
-        return (ptr >= & __bss_end__) && (ptr < & __StackLimit);
+        return (ptr >= heapEnd) && (ptr < & __StackLimit);
     }
 
+    bool memoryIsInArena(void * ptr) {
+        return (ptr >= & __bss_end__) && (ptr < arenaEnd);
+    }
+
+    // TODO remove
     bool memoryIsInCurrentArena(void * ptr) {
-        return (ptr >= arena) && (ptr < & __StackLimit);
+        return true;
     }
 
+    // TODO remove
     bool memoryInsideArena() {
         return ! memoryIsInCurrentArena(& __bss_end__);
     }
 
     void memoryEnterArena() {
-        arena = new (heapEnd) Arena{arena};
-        heapEnd += sizeof (Arena);
-        TRACE_MEMORY("Entering arena @" << (void*)(arena) << ", free " << memoryFreeHeap());
+        Arena * a = mallocArena<Arena>();
+        a->previous = arenaStart;
+        arenaStart = a;
+        TRACE_MEMORY("Entering arena @" << (void*)(a) << " (previous @ " << (void*)a->previous <<", free heap " << memoryFreeHeap() << ")");
     }
 
     void memoryLeaveArena() {
-        TRACE_MEMORY("Leaving arena @" << (void*)(arena) << ", free " << memoryFreeHeap());
-        // TODO BSOD in debug mode, do nothing in production
-        if (arena->previous == nullptr)
-            return;
-        heapEnd = reinterpret_cast<char*>(arena);
-        arena = arena->previous;
-        TRACE_MEMORY("Current arena @" << (void*)(arena) << ", free " << memoryFreeHeap());
+        ASSERT(arenaStart != nullptr); 
+        Arena * old = arenaStart;
+        (void)old; // ignore the warning of unused variable when not tracing memory 
+        arenaEnd = (char*) arenaStart;
+        arenaStart = arenaStart->previous;
+        TRACE_MEMORY("Leaving arena @" << (void*)(old) << " (previous @ " << (void*)arenaStart <<", free heap " << memoryFreeHeap() << ")");
+    }
+
+    void * mallocArena(size_t numBytes) {
+        void * result = arenaEnd;
+        arenaEnd += numBytes;
+        // if we are over the limit, panic
+        ASSERT(arenaEnd <= heapEnd);
+        return result;
     }
 
     void * malloc(size_t numBytes) { return __wrap_malloc(numBytes); }
