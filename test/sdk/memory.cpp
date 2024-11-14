260692c143f98b6226357743503ccbb6d5fd6c52
zduka <zduka@outlook.com>
2024-11-14 21:23:24 +0100

    Nicer memory management with Heap and Arena static classes.

--- test/sdk/memory.cpp
+++ test/sdk/memory.cpp
@@ -1,46 +1,37 @@
 #include <platform/tests.h>
 #include <rckid/rckid.h>
 
+
 TEST(memory, arenaLeave) {
-    uint32_t freeHeap = rckid::memoryFreeHeap();
-    rckid::memoryEnterArena();
-    uint32_t afterArena = rckid::memoryFreeHeap();
+    using namespace rckid;
+    uint32_t freeHeap = memoryFree();
+    Arena::enter();
+    uint32_t afterArena = memoryFree();
     // entering arena allocated new arena header, total free memory should be lower by that
     EXPECT(freeHeap - sizeof(void*) == afterArena);
-    rckid::mallocArena(128);
-    uint32_t afterAlloc = rckid::memoryFreeHeap();
+    Arena::malloc(128);
+    uint32_t afterAlloc = memoryFree();
     // the 128bytes, no header
     EXPECT(afterArena - 128 == afterAlloc);
-    rckid::memoryLeaveArena();
+    Arena::leave();
     // everything gets deleted when done
-    EXPECT(freeHeap == rckid::memoryFreeHeap());
+    EXPECT(freeHeap == memoryFree());
 }
 
 TEST(memory, immediateDeleteDeallocates) {
-    uint32_t freeHeap = rckid::memoryFreeHeap();
+    uint32_t freeHeap = rckid::memoryFree();
     void * ptr = rckid::malloc(128);
-    EXPECT(rckid::memoryFreeHeap() + 128 + 4 == freeHeap);
+    EXPECT(rckid::memoryFree() + 128 + 4 == freeHeap);
     rckid::free(ptr);
-    EXPECT(rckid::memoryFreeHeap() == freeHeap);
+    EXPECT(rckid::memoryFree() == freeHeap);
 }
 
 TEST(memory, nonImmediateDeleteDoesNotDeallocate) {
-    uint32_t freeHeap = rckid::memoryFreeHeap();
+    uint32_t freeHeap = rckid::memoryFree();
     void * ptr1 = rckid::malloc(128);
     void * ptr2 = rckid::malloc(128);
-    EXPECT(rckid::memoryFreeHeap() + (128 + 4) * 2 == freeHeap);
+    EXPECT(rckid::memoryFree() + (128 + 4) * 2 == freeHeap);
     rckid::free(ptr1);
     rckid::free(ptr2);
-    EXPECT(rckid::memoryFreeHeap() != freeHeap);
+    EXPECT(rckid::memoryFree() != freeHeap);
 }
-
-TEST(memory, freeAndUsedCountersMatch) {
-    uint32_t totalMem = rckid::memoryFreeHeap() + rckid::memoryUsedHeap();
-    EXPECT(totalMem = rckid::memoryFreeHeap() + rckid::memoryUsedHeap());
-    void * ptr = rckid::malloc(128);
-    EXPECT(totalMem = rckid::memoryFreeHeap() + rckid::memoryUsedHeap());
-    rckid::free(ptr);
-    EXPECT(totalMem = rckid::memoryFreeHeap() + rckid::memoryUsedHeap());
-    EXPECT(totalMem = rckid::memoryFreeHeap() + rckid::memoryUsedHeap());
-}
-
