1cb53623bd2878f40c389fed0c75c9727cb41d5b
zduka <zduka@outlook.com>
2025-02-15 23:47:51 +0100

    towards mk3 build

--- sdk/backends/mk3/mk3.cpp
+++ sdk/backends/mk3/mk3.cpp
@@ -9,35 +9,217 @@
 
 #include "rckid/rckid.h"
 
-
-// PICO platform sets
 namespace rckid {
 
-    void initialize() {
+    // forward declaration of the bsod function
+    NORETURN(void bsod(uint32_t error, uint32_t line = 0, char const * file = nullptr));
 
-    }
-
-    void initialize() {
+    namespace filesystem {
+        void initialize();
     }
 
     void fatalError(uint32_t error, uint32_t line, char const * file) {
-        // clear all memory arenas to clean up space, this is guarenteed to succeed as the SDK creates memory arena when it finishes initialization    
-        /**
-        memoryReset();
-        bsod(error, line, file, nullptr);
-        systemMalloc_ = true;
-        if (sdIso_.good())
-            sdIso_.close();
-        if (flashIso_.good())
-            flashIso_.close();
-        while (! WindowShouldClose())
-            PollInputEvents();
-        systemMalloc_ = false;
-        */
-        std::exit(EXIT_FAILURE);
+        UNIMPLEMENTED;
     }
 
+    Writer debugWrite() {
+        UNIMPLEMENTED;
+    }
 
+    void initialize([[maybe_unused]] int argc, [[maybe_unused]] char const * argv[]) {
+        UNIMPLEMENTED;
+    }
 
+    void tick() {
+        UNIMPLEMENTED;
+    }
+
+    void yield() {
+        UNIMPLEMENTED;
+    }
+
+    uint32_t uptimeUs() {
+        UNIMPLEMENTED;
+    }
+
+    // io
+
+    bool btnDown(Btn b) {
+        UNIMPLEMENTED;
+    }
+
+    bool btnPressed(Btn b) {
+        UNIMPLEMENTED;
+    }
+
+    bool btnReleased(Btn b) {
+        UNIMPLEMENTED;
+    }
+
+    int16_t accelX() {
+        UNIMPLEMENTED;
+    }
+
+    int16_t accelY() {
+        UNIMPLEMENTED;
+    }
+
+    int16_t accelZ() {
+        UNIMPLEMENTED;
+    }
+
+    int16_t gyroX() {
+        UNIMPLEMENTED;
+    }
+
+    int16_t gyroY() {
+        UNIMPLEMENTED;
+    }
+
+    int16_t gyroZ() {
+        UNIMPLEMENTED;
+    }
+
+    uint16_t lightAmbient() {
+        UNIMPLEMENTED;        
+    }
+    
+    uint16_t lightUV() {
+        UNIMPLEMENTED;
+    }
+
+    // display
+
+    DisplayRefreshDirection displayRefreshDirection() {
+        UNIMPLEMENTED;
+    }
+
+    void displaySetRefreshDirection(DisplayRefreshDirection value) {
+        UNIMPLEMENTED;
+    }
+
+    uint8_t displayBrightness() {
+        UNIMPLEMENTED;
+    }
+
+    void displaySetBrightness(uint8_t value) {
+        UNIMPLEMENTED;
+    }
+
+    Rect displayUpdateRegion() {
+        UNIMPLEMENTED;
+    }
+
+    void displaySetUpdateRegion(Rect value) {
+        UNIMPLEMENTED;
+    }
+
+    bool displayUpdateActive() {
+        UNIMPLEMENTED;
+    }
+
+    void displayWaitUpdateDone() {
+        UNIMPLEMENTED;
+    }
+
+    void displayWaitVSync() {
+        UNIMPLEMENTED;
+    }
+
+    void displayUpdate(uint16_t const * pixels, uint32_t numPixels, DisplayUpdateCallback callback) {
+        UNIMPLEMENTED;
+    }
+
+    void displayUpdate(uint16_t const * pixels, uint32_t numPixels) {
+        UNIMPLEMENTED;
+    }
+
+    // audio
+
+    void audioStreamRefill(void * buffer, unsigned int samples) {
+        UNIMPLEMENTED;
+    }
+
+    bool audioHeadphones() {
+        UNIMPLEMENTED;
+    }
+
+    bool audioPaused() {
+        UNIMPLEMENTED;
+    }
+
+    bool audioPlayback() {
+        UNIMPLEMENTED;
+    }
+
+    bool audioRecording() {
+        UNIMPLEMENTED;
+    }
+
+    uint8_t audioVolume() {
+        UNIMPLEMENTED;
+    }
+
+    void audioSetVolume(uint8_t value) {
+        UNIMPLEMENTED;
+    }
+
+    void audioPlay(DoubleBuffer<int16_t> & buffer, uint32_t sampleRate, AudioCallback cb) {
+        UNIMPLEMENTED;
+    }
+
+    void audioPause() {
+        UNIMPLEMENTED;
+    }
+
+    void audioResume() {
+        UNIMPLEMENTED;
+    }
+
+    void audioStop() {
+        UNIMPLEMENTED;
+    }
+
+    // SD Card access
+
+    uint32_t sdCapacity() {
+        UNIMPLEMENTED;
+    }
+
+    bool sdReadBlocks(uint32_t start, uint8_t * buffer, uint32_t numBlocks) {
+        UNIMPLEMENTED;
+    }
+
+    bool sdWriteBlocks(uint32_t start, uint8_t const * buffer, uint32_t numBlocks) {
+        UNIMPLEMENTED;
+    }
+
+    // Cartridge filesystem access
+
+    uint32_t cartridgeCapacity() { 
+        UNIMPLEMENTED;
+    }
+
+    uint32_t cartridgeWriteSize() { 
+        UNIMPLEMENTED;
+        return 256; 
+    }
+
+    uint32_t cartridgeEraseSize() { 
+        UNIMPLEMENTED;
+        return 4096; 
+    }
+
+    void cartridgeRead(uint32_t start, uint8_t * buffer, uint32_t numBytes) {
+        UNIMPLEMENTED;
+    }
+
+    void cartridgeWrite(uint32_t start, uint8_t const * buffer) {
+        UNIMPLEMENTED;
+    }
+
+    void cartridgeErase(uint32_t start) {
+        UNIMPLEMENTED;
+    }
 
 }
