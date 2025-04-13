99bcb25a4c3f5a0f94bcda31689956620341a405
zduka <zduka@outlook.com>
2025-04-13 20:09:06 +0200

    Very basic font tile generator.

--- sdk/rckid/apps/MainMenu.h
+++ sdk/rckid/apps/MainMenu.h
@@ -1,13 +1,22 @@
 #pragma once
 
 #include "../ui/form.h"
+#include "../ui/label.h"
+#include "../ui/carousel.h"
+#include "../ui/menu.h"
 
 namespace rckid {
 
-    /** Main menu application.  */
+    /** Main menu application. 
+     */
     class MainMenu : public ui::App {
     public:
+        MainMenu() : ui::App{} {
 
+        }
+
+    private:
+        ui::CarouselMenu * c_;
 
 
     }; // rckid::MainMenu
