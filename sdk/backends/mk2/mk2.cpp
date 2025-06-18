5bd1b0da7d90eea605009d8d4c50e523c1ee9354
zduka <zduka@outlook.com>
2025-06-18 21:47:09 +0200

    Arena allocation on device fixed.

--- sdk/backends/mk2/mk2.cpp
+++ sdk/backends/mk2/mk2.cpp
@@ -25,6 +25,7 @@ extern "C" {
 #include "sd/sd.h"
 #include "rckid/rckid.h"
 #include "rckid/filesystem.h"
+#include "rckid/ui/header.h"
 
 #include "avr/src/commands.h"
 #include "avr/src/status.h"
@@ -40,14 +41,20 @@ extern "C" {
         return -1;
     }    
 
-    void *__wrap_malloc(size_t numBytes) { return rckid::Heap::allocBytes(numBytes); }
+    void *__wrap_malloc(size_t numBytes) { 
+        if (rckid::Heap::isDefaultTarget())
+            return rckid::Heap::allocBytes(numBytes); 
+        else 
+            return rckid::Arena::allocBytes(numBytes);
+    }
+
     void __wrap_free(void * ptr) { 
         if (rckid::Heap::contains(ptr))
             rckid::Heap::free(ptr); 
     }
 
     void *__wrap_calloc(size_t numBytes) {
-        void * result = rckid::Heap::allocBytes(numBytes);
+        void * result = __wrap_malloc(numBytes);
         memset(result, 0, numBytes);
         return result;
     }
@@ -434,6 +441,7 @@ namespace rckid {
                 time::idleTimeout_ = RCKID_IDLE_TIMETOUT;
                 time::idleTimeoutKeepalive_ = RCKID_IDLE_TIMETOUT_KEEPALIVE;
             }
+            ui::Header::refresh();
         }
         ++io::ticks_;
         // make sure the I2C is off, then set it up so that it can talk to the accelerometer
