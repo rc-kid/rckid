9047713c39b41ca78f271fd64303c1bba887896f
zduka <zduka@outlook.com>
2025-08-20 22:48:19 +0200

    Tetris modulo audio & hall of fame ported.

--- sdk/rckid/apps/games/Tetris.h
+++ sdk/rckid/apps/games/Tetris.h
@@ -2,6 +2,7 @@
 #include "../../app.h"
 #include "../../graphics/canvas.h"
 #include "../../assets/fonts/Iosevka24.h"
+#include "../../assets/fonts/OpenDyslexic24.h"
 #include "../../assets/fonts/MetalLord64.h"
 
 namespace rckid {
@@ -23,42 +24,133 @@ namespace rckid {
     protected:
 
         void update() override {
-            for (unsigned i = 0; i < NUM_FALLING_PIECES; ++i) {
-                fallingY_[i] += 1;
-                if (fallingY_[i] == 240) {
-                    fallingY_[i] = - 40;
-                    fallingX_[i] = random() % 320;
-                    fallingPieces_[i].randomize();
+            switch (mode_) {
+                case Mode::Intro:
+                    if (btnPressed(Btn::A) || btnPressed(Btn::Start))  {
+                        resetGame();
+                        mode_ = Mode::Game;
+                    }
+                    for (unsigned i = 0; i < NUM_FALLING_PIECES; ++i) {
+                        fallingY_[i] += 1;
+                        if (fallingY_[i] == 240) {
+                            fallingY_[i] = - 40;
+                            fallingX_[i] = random() % 320;
+                            fallingPieces_[i].randomize();
+                        }
+                        if (random() % 1000 < 10)
+                            fallingPieces_[i] = fallingPieces_[i].rotate();
+                    }
+                    break;
+                case Mode::Game: {
+                    bool goDown = false;
+                    if (--countdown_ == 0) {
+                        goDown = true && (level_ != 1);
+                        countdown_ = speed_;
+                    }
+                    //if (btnPressed(Btn::A))
+                    //    runModal<Alert>("PAUSE", "Press A to continue...");
+                    // TODO if we should, go down automatically
+                    if (btnPressed(Btn::Up))
+                        rotate();
+                    if (btnDown(Btn::Down)) {
+                        if (allowDown_) {
+                            goDown = true;
+                            ++score_;
+                        }
+                    } else {
+                        allowDown_ = true;
+                    }
+                    // check left & right
+                    if (btnPressed(Btn::Left)) {
+                        if (validate(cur_, x_ - 1, y_))
+                            --x_;
+                        // else rumble
+                    }
+                    if (btnPressed(Btn::Right)) {
+                        if (validate(cur_, x_ + 1, y_))
+                            ++x_;
+                        // else rumble
+                    }
+                    if (goDown) {
+                        if (validate(cur_, x_, y_ + 1)) {
+                            ++y_;
+                            //audio_[0].setFrequency(220, 5);
+                        } else {
+                            addToGrid(cur_, x_, y_);
+                            compactRows(y_);
+                            spawn();
+                            if (!validate(cur_, x_, y_))
+                                gameOver();
+                        }
+                    }
+                    if (btnPressed(Btn::B)) {
+                        btnClear(Btn::B);
+                        // check if we should save the high score
+                        //shouldSaveHighScore();
+                        mode_ = Mode::Intro;
+                        //modeTimeout_ = INTRO_FRAMES_LENGTH;
+                    }
+
+                    break;
                 }
-                if (random() % 1000 < 10)
-                    fallingPieces_[i] = fallingPieces_[i].rotate();
             }
+            
             CanvasApp<ColorRGB>::update();
         }
 
         void draw() override {
             g_.fill();
+            switch (mode_) {
+                case Mode::Intro: {
+                    drawIntroFallingPieces();
+                    Font const & f = Font::fromROM<assets::MetalLord64>();
+                    Font const & f2 = Font::fromROM<assets::Iosevka24>();
 
-            drawIntroFallingPieces();
-            Font const & f = Font::fromROM<assets::MetalLord64>();
-            Font const & f2 = Font::fromROM<assets::Iosevka24>();
+                    int tw = f.textWidth("TETRIS");
+                    g_.textRainbow(160 - tw / 2, 30, f, startHue_, 1024) << "TETRIS";
 
-            int tw = f.textWidth("TETRIS");
-            g_.textRainbow(160 - tw / 2, 30, f, startHue_, 1024) << "TETRIS";
-
-            tw = f2.textWidth("Press A to start");
-            g_.textRainbow(160 - tw / 2, 160, f2, startHue_, -1024) << "Press A to start";
-            startHue_ += 128;
-
+                    tw = f2.textWidth("Press A to start");
+                    g_.textRainbow(160 - tw / 2, 160, f2, startHue_, -1024) << "Press A to start";
+                    startHue_ += 128;
+                    break;
+                }
+                case Mode::Game: {
+                    int startX = (320 - PLAY_WIDTH * 10) / 2;
+                    int startY = 238 - PLAY_HEIGHT * 10;
+                    g_.fill(ColorRGB::DarkGray(), Rect::XYWH(startX - 2, startY-2, 10 * PLAY_WIDTH + 3, 10 * PLAY_HEIGHT + 3));
+                    // draw the playgrid
+                    for (int y = 0; y < PLAY_HEIGHT; ++y)
+                        for (int x = 0; x < PLAY_WIDTH; ++x)
+                            drawTile(startX + x * 10, startY + y * 10, grid(x, y));
+                    // draw the current tetromino
+                    drawTetromino(startX + x_ * 10, startY + y_ * 10, cur_);
+                    drawTetromino(250, 100, next_);
+                    g_.setFont(Font::fromROM<assets::OpenDyslexic24>());
+                    g_.setFg(ColorRGB::Gray());
+                    g_.text(0, 20) << "Score:";
+                    g_.text(0, 60) << "Level:";
+                    g_.setFg(ColorRGB::White());
+                    String score{STR(score_)};
+                    g_.text(startX - 20 - g_.font().textWidth(score), 40) << score;
+                    String level{STR(level_)};
+                    g_.text(startX - 20 - g_.font().textWidth(level), 80) << level;
+                    break;
+                }
+            }
             CanvasApp<ColorRGB>::draw();
         }
-    
+
     private:
 
         static constexpr uint8_t NUM_COLORS = 6;
 
         static constexpr Coord PLAY_WIDTH = 10;
         static constexpr Coord PLAY_HEIGHT = 22;
+
+        enum class Mode {
+            Intro, 
+            Game,
+        }; 
 
         /** Tetromino. 
          
@@ -120,6 +212,30 @@ namespace rckid {
             uint8_t grid_[4][4];
         }; 
 
+        void resetGame() {
+            level_ = 1;
+            score_ = 0;
+            speed_ = getLevelSpeed(1);
+            for (unsigned i = 0; i < PLAY_WIDTH * PLAY_HEIGHT; ++i)
+                grid_[i] = 0;
+            next_.randomize();
+            spawn();
+            countdown_ = speed_;
+            mode_ = Mode::Intro;
+            //modeTimeout_ = INTRO_FRAMES_LENGTH;
+        }
+
+        /** Game over.
+
+            Display game over dialg and then reset the game. 
+
+            TODO store high score, etc.          
+         */
+        void gameOver() {
+            //runModal<Alert>("GAME OVER", "Press A to continue...", ColorRGB{32, 0, 0});
+            //shouldSaveHighScore();
+            resetGame();
+        }
 
         uint8_t grid(int x, int y) {
             // return the unattainable 0xff color which will stop rotations or falls when out of bounds
@@ -163,19 +279,125 @@ namespace rckid {
                 }
         }
 
+        /** Spawns next tetromino. 
+         */
+        void spawn() {
+            cur_ = next_;
+            next_.randomize();
+            x_ = 3;
+            if (cur_.kind() == Tetromino::Kind::Beam || cur_.kind() == Tetromino::Kind::Square)
+                y_ = -1;
+            else 
+                y_ = 0;
+            // don't allow the next tile to go immediately down
+            allowDown_ = false;
+        }
 
+        /** Rotates the current tetromino, if possible.
+         */
+        void rotate() {
+            Tetromino tr = cur_.rotate();
+            if (validate(tr, x_, y_))
+                cur_ = tr;
+            // TODO else rumble bad
+            //else
+            //   rumble()
+            //audio_[1].setFrequency(440, 30);
+           rumblerEffect(RumblerEffect::Nudge());
+        }
 
+        void addToGrid(Tetromino const & t, int x, int y) {
+            for (int ty = 0; ty < 4; ++ty)
+                for (int tx = 0; tx < 4; ++tx) {
+                    uint8_t c = t.grid(tx, ty);
+                    if (c != 0)
+                        setGrid(x + tx, y + ty, c);
+                }
+        }
+
+        /** Checks row compacting when the tile lands. 
+         */
+        void compactRows(int startRow) {
+            uint32_t compacted = 0;
+            for (int y = startRow; y < startRow + 4; ++y) {
+                bool compact = true;
+                for (int x = 0; x < PLAY_WIDTH; ++x) {
+                    uint8_t gc = grid(x, y);
+                    if (gc == 0 || gc == 0xff) {
+                        compact = false;
+                        break;
+                    }
+                }
+                if (compact) {
+                    ++compacted;
+                    for (int yy = y; yy > 1; --yy)
+                        for (int x = 0; x < PLAY_WIDTH; ++x)
+                            setGrid(x, yy, grid(x, yy - 1));
+                    for (int x = 0; x < PLAY_WIDTH; ++x)
+                        setGrid(x, 0, 0);
+                }
+            }
+            switch (compacted) {
+                case 1:
+                    score_ += 100 * level_;
+                    //audio_[3].setFrequency(220, 250);
+                    break;
+                case 2:
+                    score_ += 300 * level_;
+                    //audio_[3].setFrequency(330, 500);
+                    break;
+                case 3:
+                    score_ += 500 * level_;
+                    //audio_[3].setFrequency(440, 750);
+                    break;
+                case 4:
+                    score_ += 800 * level_;
+                    //audio_[3].setFrequency(660, 1000);
+                    break;
+                default:
+                    // no points awarded
+                    break;
+            }
+            levelCompacted_ += compacted;
+            if (levelCompacted_ > 10) {
+                ++level_;
+                speed_ = getLevelSpeed(level_);
+                levelCompacted_ -= 10;
+                rumblerEffect(RumblerEffect::OK());
+            } else if (compacted) {
+                rumblerEffect(RumblerEffect::Nudge());
+            }
+        }
 
         void drawIntroFallingPieces() {
             for (unsigned i = 0; i < NUM_FALLING_PIECES; ++i)
                 drawTetromino(fallingX_[i], fallingY_[i], fallingPieces_[i]);
         }
 
+        Mode mode_ = Mode::Intro;
+
         /** The play area.  
          
             Stored value indicates color, 0 for black, which means empty. 
          */
         uint8_t grid_[PLAY_WIDTH * PLAY_HEIGHT]; 
+
+        Tetromino cur_;
+        Coord x_;
+        Coord y_;
+        Tetromino next_;
+
+        uint32_t level_ = 1;
+        uint32_t levelCompacted_ = 0;
+        uint32_t score_ = 0;
+        uint32_t speed_;
+        uint32_t countdown_;
+        // used to disable immediate go down of spawned tiles after previoius one has been landed
+        bool allowDown_ = true;
+
+
+
+
 
 
         // falling tetrominos in the intro
@@ -248,6 +470,33 @@ namespace rckid {
             ColorRGB::Cyan(),
         };
 
+        static uint32_t getLevelSpeed(uint32_t level) {
+            switch (level) {
+                case 1: return 60;
+                case 2: return 53;
+                case 3: return 49;
+                case 4: return 45;
+                case 5: return 41;
+                case 6: return 37;
+                case 7: return 33;
+                case 8: return 28;
+                case 9: return 22;
+                case 10: return 17;
+                case 11: return 11;
+                case 12: return 10;
+                case 13: return 9;
+                case 14: return 8;
+                case 15: return 7;
+                case 16: return 6;
+                case 17: return 6;
+                case 18: return 5;
+                case 19: return 5;
+                case 20: return 4;
+                case 21: return 4;
+                default:
+                    return 3;
+            }
+        }
 
     }; // rckid::Tetris
 
