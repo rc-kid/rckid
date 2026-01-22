3e8c59c137f0f7c814e884955e36ca1df4c8ed9c
zduka <zduka@outlook.com>
2026-01-22 23:54:43 +0100

    immutable_ptr and mutable_ptr

--- sdk2/test/memory.cpp
+++ sdk2/test/memory.cpp
@@ -1,15 +1,94 @@
 #include <platform/tests.h>
 #include <rckid/memory.h>
 
+namespace {
 
-uint8_t const x[] = { 1, 2, 3, 4 };
-uint8_t y[] = { 1, 2, 3, 4 };
+    class Foobar {
+    public:
+
+        uint32_t value = 0;
+
+    } __attribute__((packed)); 
+
+    uint8_t const x[] = { 1, 2, 3, 4 };
+    uint8_t y[] = { 1, 2, 3, 4 };
+    uint8_t const data[] = { 10, 20, 30, 40, 50 };
+
+    char const * str = "Literal";
+
+} // anonymous namespace
+
 
 TEST(memory, immutable) {
     EXPECT(sizeof(x) == 4);
     EXPECT(sizeof(y) == 4);
     EXPECT(rckid::hal::memory::isImmutableDataPtr(& x) == true);
     EXPECT(rckid::hal::memory::isImmutableDataPtr(& y) == false);
+}
+
+
+TEST(memory, noArrayCookiesOnPODS) {
+    using namespace rckid;
+    Heap::UseAndReserveGuard h;
+    Foobar * array = new Foobar[3];
+    EXPECT(h.usedDelta() == 4 + 3 * 4);
+    EXPECT(h.reservedDelta() == 4 + 3 * 4);
+    delete [] array;
+    EXPECT(h.usedDelta() == 0);
+    EXPECT(h.reservedDelta() == 0);
+}
+
+
+TEST(memory, immutable_ptr) {
+    using namespace rckid;
+    Heap::UseAndReserveGuard h;
+    EXPECT(hal::memory::isImmutableDataPtr(data) == true);
+    {
+        immutable_ptr<uint8_t> p1{data};
+        EXPECT(h.usedDelta() == 0);
+        EXPECT(h.reservedDelta() == 0);
+        EXPECT(p1[0] == 10);
+        EXPECT(p1[4] == 50);
+    }
+}
+
+TEST(memory, mutable_ptr) {
+    using namespace rckid;
+    Heap::UseAndReserveGuard h;
+    {
+        mutable_ptr<uint8_t> p1{data};
+        EXPECT(h.usedDelta() == 0);
+        EXPECT(h.reservedDelta() == 0);
+        EXPECT(p1.ptr()[0] == 10);
+        EXPECT(p1.ptr()[4] == 50);
+        uint8_t * mutData = p1.mut();
+        EXPECT(h.usedDelta() == 16);
+        EXPECT(h.reservedDelta() == 16);
+        mutData[0] = 100;
+        mutData[4] = 200;
+        EXPECT(p1.ptr()[0] == 100);
+        EXPECT(p1.ptr()[4] == 200);
+    }
+    EXPECT(h.usedDelta() == 0);
+    EXPECT(h.reservedDelta() == 0);
+}
+
+TEST(memory, mutable_ptr_string) {
+    using namespace rckid;
+    Heap::UseAndReserveGuard h;
+    {
+        mutable_ptr<char> p1{str};
+        EXPECT(h.usedDelta() == 0);
+        EXPECT(h.reservedDelta() == 0);
+        EXPECT(p1.count() == 8);
+        char * mutData = p1.mut();
+        EXPECT(h.usedDelta() == 16);
+        EXPECT(h.reservedDelta() == 16);
+        mutData[0] = 'l';
+        EXPECT(p1.ptr()[0] == 'l');
+    }
+    EXPECT(h.usedDelta() == 0);
+    EXPECT(h.reservedDelta() == 0);
 }
 
 TEST(memory, heapAlloc) {
