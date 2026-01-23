a541a78c947a1123bfe8fd26a7525ebacdd881c5
zduka <zduka@outlook.com>
2026-01-23 09:14:41 +0100

    String implemented.
    Also clone() method for mutable_ptr.

--- sdk2/include/rckid/string.h
+++ sdk2/include/rckid/string.h
@@ -1,8 +1,101 @@
 #pragma once
+
 #include <platform/writer.h>
 
 #include <rckid/memory.h>
 
 namespace rckid {
 
+    /** String is a very simple view over characters. 
+     
+     */
+    class String {
+    public:
+
+        String() = default;
+        constexpr String(char const * s) : data_{s} { }
+
+        ~String() = default;
+
+        String(String const & other): data_{other.data_.clone()} {}
+
+        String(String && other) noexcept : data_{std::move(other.data_)} {}
+        
+        String & operator = (char const * s) {
+            data_ = s;
+            return *this;
+        }
+
+        String & operator = (String const & other) {
+            if (this == & other)
+                return *this;
+            data_ = other.data_.clone();
+            return *this;
+        }
+
+        String & operator = (String && other) noexcept {
+            if (this == & other)
+                return *this;
+            data_ = std::move(other.data_);
+            return *this;
+        }
+
+        char const * c_str() const {
+            return data_.ptr();
+        }
+
+        char const * data() const {
+            return data_.ptr();
+        }
+
+        uint32_t size() const {
+            return data_.count() ? data_.count() - 1 : 0;
+        }
+
+        bool operator == (String const & other) const {
+            return std::strcmp(data_.ptr(), other.data_.ptr()) == 0;
+        }
+
+        bool operator != (String const & other) const {
+            return std::strcmp(data_.ptr(), other.data_.ptr()) != 0;
+        }
+
+        bool operator < (String const & other) const {
+            return std::strcmp(data_.ptr(), other.data_.ptr()) < 0;
+        }
+
+        bool operator > (String const & other) const {
+            return std::strcmp(data_.ptr(), other.data_.ptr()) > 0;
+        }
+
+        bool operator <= (String const & other) const {
+            return std::strcmp(data_.ptr(), other.data_.ptr()) <= 0;
+        }
+
+        bool operator >= (String const & other) const {
+            return std::strcmp(data_.ptr(), other.data_.ptr()) >= 0;
+        }
+
+        /** Creates new string by concatenating two existing ones. 
+         
+            Note that this is a convenience function only - for any non-trivial string manipulations the string builder writer interface (STR macro) should be used for better ergonomics *and* performance.
+         */
+        String operator + (String const & other) const {
+            uint32_t newSize = size() + other.size();
+            char * newData = new char[newSize + 1];
+            std::memcpy(newData, data_.ptr(), size());
+            std::memcpy(newData + size(), other.data_.ptr(), other.size());
+            newData[newSize] = '\0';
+            return String{newData, newSize + 1};
+        }
+
+    private:
+
+        // private constructor to utilize the already calculated size
+        String(char * s, uint32_t count) : data_{s, count} { }
+    
+        // we are using mutable_ptr as it provides size tracking already
+        mutable_ptr<char> data_;
+
+    }; // rckid::String
 } // namespace rckid
---
+++ sdk2/test/string.cpp
@@ -0,0 +1,65 @@
+#include <platform/tests.h>
+
+#include <rckid/string.h>
+
+namespace {
+
+    char const * foo = "foo";
+    char const * bar = "bar";
+
+} // anonymous namespace
+
+TEST(string, fromLiteral) {
+    using namespace rckid;
+    Heap::UseAndReserveGuard g_;
+    String s{foo};
+    EXPECT(s.size() == 3);
+    EXPECT(s.c_str() == foo);
+    EXPECT(g_.usedDelta() == 0);
+    EXPECT(g_.reservedDelta() == 0);
+}
+
+TEST(string, literalClone) {
+    using namespace rckid;
+    Heap::UseAndReserveGuard g_;
+    String s1{foo};
+    String s2 = s1;
+    EXPECT(s2.size() == 3);
+    EXPECT(s2.size() == 3);
+    EXPECT(s1.c_str() == foo);
+    EXPECT(s2.c_str() == foo);
+    EXPECT(g_.usedDelta() == 0);
+    EXPECT(g_.reservedDelta() == 0);
+}
+
+TEST(string, compare) {
+    using namespace rckid;
+    Heap::UseAndReserveGuard g_;
+    String s1{foo};
+    String s2 = s1;
+    EXPECT(s1 == foo);
+    EXPECT(s1 == s2);
+    EXPECT(s1 <= s2);
+    EXPECT(s2 >= s1);
+    s2 = bar;
+    EXPECT(g_.usedDelta() == 0);
+    EXPECT(g_.reservedDelta() == 0);
+    EXPECT(s2 == bar);
+    EXPECT(s1 != s2);
+    EXPECT(s1 > s2);
+    EXPECT(s2 < s1);
+    EXPECT(s1 >= s2);
+    EXPECT(s2 <= s1);
+}
+
+TEST(string, addition) {
+    using namespace rckid;
+    Heap::UseAndReserveGuard g_;
+    String s1{foo};
+    String s2 = s1 + " " + bar;
+    EXPECT(s2.size() == 7);
+    EXPECT(std::strcmp(s2.c_str(), "foo bar") == 0);
+    EXPECT(g_.usedDelta() == 16);
+    EXPECT(g_.reservedDelta() == 32); // because of the intermediate allocation in the addition
+}
+
