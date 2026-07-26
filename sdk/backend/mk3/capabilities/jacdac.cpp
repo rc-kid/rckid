3afa6e1527207c66ce07b91ff6bab014435f5b1b
zduka <zduka@outlook.com>
2026-07-26 18:01:09 +0200

    Updates to the jacdac driver.

--- sdk/backend/mk3/capabilities/jacdac.cpp
+++ sdk/backend/mk3/capabilities/jacdac.cpp
@@ -33,8 +33,8 @@ namespace rckid {
 
         void __not_in_flash_func(jacdacRxDone)() {
             pio_interrupt_clear(JACDAC_PIO, 0);
-            // by the time we get here we can assume that the DMA has transferred everything we need, no need to wait for anything. Store the size of the received frame in the back buffer
-            instance_->rxBuffer.back().setUsed(dma_hw->ch[instance_->dma].transfer_count);
+            // by the time we get here we can assume that the DMA has transferred everything we need, no need to wait for anything. Store the size of the received frame in the back buffer (transfer count decrements from the initial value, which we know was set to frame size)
+            instance_->rxBuffer.back().setUsed(sizeof(Jacdac::Frame) - dma_hw->ch[instance_->dma].transfer_count);
             // swap front & back buffers and set the rxReady flag (only if the front buffer is already processed)
             if (instance_->rxReady == false) {
                 instance_->rxBuffer.swap();
@@ -103,7 +103,8 @@ namespace rckid {
         channel_config_set_dreq(&c, pio_get_dreq(JACDAC_PIO, instance_->sm, true));
         channel_config_set_read_increment(&c, true);
         channel_config_set_write_increment(&c, false);
-        dma_channel_configure(instance_->dma, &c, &JACDAC_PIO->txf[instance_->sm], NULL, 0, false);
+        // write addressis the TX FIFO, read address is null here, set by transfer from buffer below explicitly
+        dma_channel_configure(instance_->dma, &c, &JACDAC_PIO->txf[instance_->sm], nullptr, 0, false);
         dma_channel_transfer_from_buffer_now(instance_->dma, data, numBytes);
         // start the PIO
         sws_tx_program_init(JACDAC_PIO, instance_->sm, instance_->txOffset, JACDAC_PIN);
@@ -136,7 +137,8 @@ namespace rckid {
         channel_config_set_dreq(&c, pio_get_dreq(JACDAC_PIO, instance_->sm, false));
         channel_config_set_read_increment(&c, false);
         channel_config_set_write_increment(&c, true);
-        dma_channel_configure(instance_->dma, &c, NULL, &JACDAC_PIO->rxf[instance_->sm], sizeof(Frame), false);
+        // write address is nullptr (will be set by transfer to buffer below), read address is the RX FIFO of the pio
+        dma_channel_configure(instance_->dma, &c, nullptr, &JACDAC_PIO->rxf[instance_->sm], sizeof(Frame), false);
         // load & configure the PIO program
         sws_rx_program_init(JACDAC_PIO, instance_->sm, instance_->rxOffset, JACDAC_PIN);
         pio_sm_set_clock_speed(JACDAC_PIO, instance_->sm, 8 * JACDAC_BAUDRATE);
